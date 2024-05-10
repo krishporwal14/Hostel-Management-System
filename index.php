11f34b0e673ce939fa73d9576e11a3c060ab8d38
Krish Porwal <101163531+krishporwal14@users.noreply.github.com>
2024-05-10 16:53:43 +0530

    Added the link to direct to power bi dashboard for data analysis.

--- index.php
+++ index.php
@@ -95,15 +95,15 @@
     ?>
     <div class="min-h-screen w-full bg-neutral-950 text-white">
         <nav class="bg-zinc-800 flex items-center p-9 justify-between">
-            <p class="text-3xl font-bold">SMJV</p>
+            <p class="text-3xl font-bold"><a href="./index.php#dashboard-container">SMJV</a></p>
             <ul class="flex text-center list-none text-xl">
-                <li class="inline-block"><a href="./forms.php" class="no-underline px-9">Forms</a></li>
-                <li class="inline-block"><a href="./index.php#dashboard-container" class="no-underline px-9">Dashboard</a></li>
-                <li class="inline-block"><a href="./displays/hostel_branch.php" class="no-underline px-9">Hostel</a></li>
-                <li class="inline-block"><a href="./displays/students.php" class="no-underline px-9">Student</a></li>
-                <li class="inline-block"><a href="./displays/employees.php" class="no-underline px-9">Employees</a></li>
-                <li class="inline-block"><a href="./displays/rooms.php" class="no-underline px-9">Rooms</a></li>
-                <li class="inline-block"><a href="./displays/fees.php" class="no-underline px-9">Fees</a></li>
+                <li class="inline-block"><a href="./forms.php" class="no-underline active:font-bold px-9">Forms</a></li>
+                <li class="inline-block"><a href="https://app.powerbi.com/groups/me/reports/2899d63e-c726-45d2-b2e7-9c63e1648ada/ReportSection?experience=power-bi" target="_blank" class="no-underline active:font-bold px-9">Dashboard</a></li>
+                <li class="inline-block"><a href="./displays/hostel_branch.php" class="no-underline active:font-bold px-9">Hostel</a></li>
+                <li class="inline-block"><a href="./displays/students.php" class="no-underline active:font-bold px-9">Student</a></li>
+                <li class="inline-block"><a href="./displays/employees.php" class="no-underline active:font-bold px-9">Employees</a></li>
+                <li class="inline-block"><a href="./displays/rooms.php" class="no-underline active:font-bold px-9">Rooms</a></li>
+                <li class="inline-block"><a href="./displays/fees.php" class="no-underline active:font-bold px-9">Fees</a></li>
             </ul>
         </nav>
 
